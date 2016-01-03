6fcda5a1ccbaef1669af79f4dc2e19bdc4198517
Alexander <sanya.matveev69@gmail.com>
2016-01-03 07:23:42 +0300

    Release

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>Just Share</title>
+		<script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+		<!-- Yes, I know, jquery is too big, but this source developed just for me -->
+		<script src="just_share.js"></script>
+		<link rel="stylesheet" href="just_share.css">
+	</head>
+	<body style="background: url(//www.hdwallpaperpc.com/preview/Buildings_Skyscrapers_Bokeh_Blur_37483/resolution_1440x900) center;">
+		<div class="just-share" style="text-align: center; margin-top: 300px;">
+			<div class="share-vkontakte"></div>
+			<div class="share-twitter"></div>
+			<div class="share-facebook"></div>
+			<div class="share-google"></div>
+		</div>
+	</body>
+<html>
