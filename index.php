dd4e26a9ae56a0e9b3b6ace38a17975908969dfc
Alexander <sanya.matveev69@gmail.com>
2016-02-07 12:57:58 +0300

    Fix directories

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 		<title>Just Share</title>
 		<script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
 		<!-- Yes, I know, jquery is too big, but this source developed just for me -->
-		<script src="just_share.js"></script>
-		<link rel="stylesheet" href="just_share.css">
+		<script src="scripts/just_share.js"></script>
+		<link rel="stylesheet" href="styles/just_share.css">
 	</head>
-	<body style="background: url(//www.hdwallpaperpc.com/preview/Buildings_Skyscrapers_Bokeh_Blur_37483/resolution_1440x900) center;">
+	<body style="background: url(styles/background.jpg) center;">
 		<div class="just-share" style="text-align: center; margin-top: 300px;">
 			<div class="share-vkontakte"></div>
 			<div class="share-twitter"></div>
